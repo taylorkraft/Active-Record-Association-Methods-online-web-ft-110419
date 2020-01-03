2e1a468356f5d6d2035caeecc5b406833e0658dd
Taylor Kraft <taylorkraft16@gmail.com>
2020-01-03 20:18:22 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -14,6 +14,7 @@ class Genre < ActiveRecord::Base
 
   def all_artist_names
     # return an array of strings containing every musician's name
-    self.artists.all[:name]
+    self.artists.collect do |a|
+      a.name
   end
 end
